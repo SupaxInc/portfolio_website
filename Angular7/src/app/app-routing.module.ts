4c4fcdceda8b36e5e42dd56799db482d828ffd31
SupaxInc <paulj0997@gmail.com>
2019-09-10 11:52:56 -0400

    Created new components and changed routing
    - Added two new pages; aboutme, home
    - Added routing to the new pages
    - Added css styles to the pages

--- Angular7/src/app/app-routing.module.ts
+++ Angular7/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagesComponent } from './pages/pages.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutmeComponent } from './pages/aboutme/aboutme.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/pages/home', pathMatch:'full'},
+
+  {
+    path: 'pages', component: PagesComponent,
+    children: [
+      // PATH URL: /pages/home
+      {path: 'home', component: HomeComponent},
+      {path: 'aboutme', component: AboutmeComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular7/src/app/app.module.ts
+++ Angular7/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutmeComponent } from './pages/aboutme/aboutme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    HomeComponent,
+    AboutmeComponent
   ],
   imports: [
     BrowserModule,
