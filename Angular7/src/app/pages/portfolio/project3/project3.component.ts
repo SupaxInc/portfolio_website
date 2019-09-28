a16870a377c3010dfcd0ac02753746480e6f423d
SupaxInc <paulj0997@gmail.com>
2019-09-28 18:17:36 -0400

    Finished project 3
    - Added/fixed css elements
    - Added HTML information

--- Angular7/src/app/pages/portfolio/project3/project3.component.ts
+++ Angular7/src/app/pages/portfolio/project3/project3.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-project3',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Project3Component implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onClick() {
+    this.router.navigate(['/pages/portfolio']);
   }
 
 }
